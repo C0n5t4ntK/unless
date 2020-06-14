5b8a0fcda73c6f99c850fd28e03451b7c9e09a9d
C0n5t4ntK <C0n5t4ntK@outlook.com>
2020-06-14 10:25:57 +0800

    Set delete cascade on comment.

--- migrations/2019-10-02-083116_create_user/up.sql
+++ migrations/2019-10-02-083116_create_user/up.sql
@@ -11,17 +11,4 @@ hobby character varying(128),
 hometown character varying(128)
 );
 
-CREATE OR REPLACE FUNCTION delete_comment_and_log_trigger() RETURNS TRIGGER AS $$
-BEGIN
-	DELETE FROM visitor_log WHERE user_id = OLD.id;
-	DELETE FROM comment WHERE user_id = OLD.id;
-	RETURN OLD;
-END;
-$$
-LANGUAGE 'plpgsql';
-
-CREATE TRIGGER delete_comment_and_log BEFORE DELETE
-	ON "user" FOR EACH ROW
-		EXECUTE PROCEDURE delete_comment_and_log_trigger();
-
 INSERT INTO "user" VALUES (1, 'C0n5t4ntK', '$2b$10$xjtIgtWBAvBxT.jEl4tdzuTz/jN6hnGwR.0/egE1nR6tan4JPb3hS', current_timestamp, current_timestamp, true, 'C0n5t4ntK@outlook.com', 'www.elapse.life', 'video_game basketball tech', 'China');
--- migrations/2019-10-06-055539_create_comment/up.sql
+++ migrations/2019-10-06-055539_create_comment/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE comment (
 id serial PRIMARY KEY,
-user_id integer NOT NULL,
-article_id integer NOT NULL,
+user_id integer REFERENCES "user"(id) ON DELETE CASCADE NOT NULL,
+article_id integer REFERENCES article(id) ON DELETE CASCADE NOT NULL,
 content text NOT NULL,
 reply_content text,
 create_time timestamp without time zone NOT NULL,
